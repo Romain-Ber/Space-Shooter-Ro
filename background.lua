d482e6558e586cdfeed70bee0dea6e43555d1f2d
Romain-Ber <romain.bernier@live.fr>
2023-02-01 14:43:21 +0100

    recent changes

--- background.lua
+++ background.lua
@@ -1,1 +1,4 @@
-
+screenNoPlayLeft = --variable definissant la zone non jouable a gauche en pixels
+screenNoPlayRight = --variable definissant la zone non jouable a droite en pixels
+screenNoPlayTop = --variable definissant la zone non jouable en haut en pixels
+screenNoPlayRight = --variable definissant la zone non jouable en bas en pixels
--- entities.lua
+++ entities.lua
@@ -1,6 +1,20 @@
-entity = {}
-entity.Width = 1 --- (retrouver la largeur d'une entite dans le texture pack)
-entity.Height = 1 --- (retrouver la hauteur d'une entite dans le texture pack)
-entity.CoordX = math.random(entity.Width / 2, (screenX - entity.Width / 2))
-entity.CoordY = entity.Height / 2
-entity.Coord = {entity.CoordX, entity.CoordY}
+function CreateEntity(arg1)
+    if arg1 = 1 then
+        local entity = {}
+        --choix du type d'entity généré--
+        local entityType = math.random(1,10) --type 1 à 10?--
+        local entityTypeGap = 50 --taille du saut en pixels entre les textures de differentes identites--
+        entity.Width =  --largeur d'une entite en fonction de entityType--
+        entity.Height =  --hauteur d'une entite en fonction de entityType--
+        local entityCoordX = math.random((entity.Width / 2 + screenNoPlayLeft), ((screenX - screenNoPlayRight) - entity.Width / 2))
+        local entityCoordY = entity.Height / 2
+        entity.Coord = {entityCoordX, entityCoordY}
+
+        --insertion de l'entite dans la collection d'identites vivantes--
+        local lastEntitiesIndex = #entities
+        entities[lastEntitiesIndex + 1] = entity
+        return entities
+    else
+        return entities
+    
+end
