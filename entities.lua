423ab12b564bcd241de85e7d7b74496c5300f27e
Romain-Ber <romain.bernier@live.fr>
2023-02-01 18:23:53 +0100

    evo

--- entities.lua
+++ entities.lua
@@ -1,20 +1,47 @@
-function CreateEntity(arg1)
-    if arg1 = 1 then
-        local entity = {}
-        --choix du type d'entity généré--
-        local entityType = math.random(1,10) --type 1 à 10?--
-        local entityTypeGap = 50 --taille du saut en pixels entre les textures de differentes identites--
-        entity.Width =  --largeur d'une entite en fonction de entityType--
-        entity.Height =  --hauteur d'une entite en fonction de entityType--
-        local entityCoordX = math.random((entity.Width / 2 + screenNoPlayLeft), ((screenX - screenNoPlayRight) - entity.Width / 2))
-        local entityCoordY = entity.Height / 2
-        entity.Coord = {entityCoordX, entityCoordY}
+---VARIABLES---
+ennemyType = 27 --nombre de types d'ennemis et de textures
+neutralType = 5 --nombre de types d'ennemis et de textures
+bonusType = 5 --nombre de types d'ennemis et de textures
 
-        --insertion de l'entite dans la collection d'identites vivantes--
-        local lastEntitiesIndex = #entities
-        entities[lastEntitiesIndex + 1] = entity
-        return entities
+---
+function entities.Load()
+
+end
+
+function entities.Update(entityType)
+    if entityType = 1 then -- si une entite est generee, executer ce code
+        local ennemy = {}
+        local ennemyPixelGap = 8 --taille du saut en pixels entre les textures de differentes identites--
+        ennemy.Width = 8 --largeur d'une entite en fonction de ennemyType--
+        ennemy.Height = 8 --hauteur d'une entite en fonction de ennemyType--
+        ennemy.CoordX = math.random((screenNoPlayLeft + 1), ((screenX - screenNoPlayRight) - ennemy.Width))
+        ennemy.CoordY = 1
+        ennemy.Type = math.random(0, ennemyType)
+        local lastEnnemyIndex = #ennemies
+        ennemies[lastEnnemyIndex + 1] = ennemy
+        return ennemies
+    elseif entityType = 2 then
+        local neutral = {}
+        local neutralPixelGap = 8 --taille du saut en pixels entre les textures de differentes identites--
+        neutral.Width = 8 --largeur d'une entite en fonction de neutralType--
+        neutral.Height = 8 --hauteur d'une entite en fonction de neutralType--
+        neutral.CoordX = math.random((screenNoPlayLeft + 1), ((screenX - screenNoPlayRight) - neutral.Width))
+        neutral.CoordY = 1
+        neutral.Type = math.random(0, neutralType)
+        local lastNeutralIndex = #neutrals
+        neutrals[lastNeutralIndex + 1] = neutral
+        return neutrals
+    elseif entityType = 3 then
+        local bonus = {}
+        local bonusPixelGap = 8 --taille du saut en pixels entre les textures de differentes identites--
+        bonus.Width = 8 --largeur d'une entite en fonction de bonusType--
+        bonus.Height = 8 --hauteur d'une entite en fonction de bonusType--
+        bonus.CoordX = math.random((screenNoPlayLeft + 1), ((screenX - screenNoPlayRight) - bonus.Width))
+        bonus.CoordY = 1
+        bonus.Type = math.random(0, bonusType)
+        local lastBonusIndex = #bonuses
+        bonuses[lastBonusIndex + 1] = bonus
+        return bonuses
     else
-        return entities
-    
+    end
 end
--- game.lua
+++ game.lua
@@ -1,28 +1,31 @@
 local Game = {}
 
-local Map_Width = 10
-local Map_Height = 10
-local Tile_Width = 70
-local Tile_Height = 70
-
-Game.Map = {}
-
-Game.TileTextures = {}
 
 
 function Game.Load()
-
+    ennemies = {}
+    neutrals = {}
+    bonuses = {}
+    print("Game: loading textures...")
+    ennemyTexturePack = love.graphics.newImage("images/ennemy.png")
+    bossTexturePack = love.graphics.newImage("images/boss.png")
+    neutralTexturePack = love.graphics.newImage("images/neutral.png")
+    bonusTexturePack = love.graphics.newImage("images/bonus.png")
+    print("Game: textures loaded!")
 end
 
 function Game.Update(dt)
-    --condition pour savoir si on cree une entite ou pas--
-    createEntity = math.random(0,1)
-    entities.CreateEntity(createEntity)
-    
+
 
 
 end
 
 function Game.Draw()
+    ennemiesNumber = #ennemies
+    for i = 1, ennemiesNumber do
+        local currentEnnemy = ennemies[i]
+        local ennemyQuad = love.graphics.newQuad(0, ennemy.Type - 1, 8, 8)
+        love.graphics.draw(ennemyTexturePack, ennemyQuad, currentEnnemy.CoordX, currentEnnemy.CoordY)
+    end
 
 return Game
