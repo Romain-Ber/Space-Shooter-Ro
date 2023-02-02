16b1d378aed3b5599115d2aeb52cc4eeeef40184
Romain-Ber <romain.bernier@live.fr>
2023-02-02 15:33:46 +0100

    v1.11

--- background.lua
+++
@@ -1,4 +0,0 @@
-screenNoPlayLeft = 100 --variable definissant la zone non jouable a gauche en pixels
-screenNoPlayRight = 100 --variable definissant la zone non jouable a droite en pixels
-screenNoPlayTop = 0 --variable definissant la zone non jouable en haut en pixels
-screenNoPlayRight= 0 --variable definissant la zone non jouable en bas en pixels
