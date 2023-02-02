85d1be102603b5fea7fe839122dfb44cda2bcc19
Romain-Ber <romain.bernier@live.fr>
2023-02-02 14:36:50 +0100

    fs

--- main.lua
+++ main.lua
@@ -1,9 +1,17 @@
 local myGame = require("game")
+setfullscreen = true
 
 function love.load()
     screenX = love.graphics.getWidth()
     screenY = love.graphics.getHeight()
     myGame.Load()
+    if(setfullscreen) then
+        love.window.setFullscreen(true)        love.window.setFullscreen(true)
+        screenWidth, screenHeight = love.window.getMode()
+        scale_x = screenWidth / 800
+        scale_y = screenHeight / 600
+    else
+    end 
 end
 
 function love.update(dt)
@@ -11,6 +19,8 @@ function love.update(dt)
 end
 
 function love.draw()
+         --full screen
+         if (setfullscreen) then love.graphics.scale(scale_x,scale_y) end
     myGame.Draw()
 end
 
